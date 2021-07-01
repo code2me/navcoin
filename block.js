6fe2deb4714cdc0ce97c6246652b59fab214407f
Navneet Bahuguna <mr.navneet.bahuguna@gmail.com>
2021-07-02 00:35:31 +0530

    >Using test driven approach >added block.js (basic structure) >added block.test.js (test file)

---
+++ block.js
@@ -0,0 +1,10 @@
+class Block {
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+}
+
+module.exports = Block;
