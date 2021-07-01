6167d6c580aa9390fab99cf5d8dee6f63038809a
Navneet Bahuguna <mr.navneet.bahuguna@gmail.com>
2021-07-02 02:41:55 +0530

    > Addded Genesis Block

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
 
 describe('Block', () => {
     const timestamp = 'a-data';
@@ -12,5 +13,17 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+    });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a Block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
     });
 });
