28d4590ef11d5ad093ee74c9ffb3c222a7145a1e
Navneet Bahuguna <mr.navneet.bahuguna@gmail.com>
2021-07-25 02:33:07 +0530

    Added Functionality to mine a block create a new block for the blockchain. developed the important sha-256 hash function. applied the hash to mine a block.

--- block.js
+++ block.js
@@ -1,4 +1,5 @@
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Block {
     constructor({timestamp, lastHash, hash, data}) {
@@ -10,6 +11,17 @@ class Block {
 
     static genesis() {
         return new this(GENESIS_DATA);
+    }
+
+    static minedBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        return new this({
+            timestamp,
+            lastHash,
+            data,
+            hash : cryptoHash(timestamp, lastHash, data)
+        });
     }
 }
 
